8af1409ebb5632222711b72780a4512445f121a1
Ayman Fathy <aimanfathi@gmail.com>
2020-12-30 00:12:38 +0200

    React Navigation Part2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import { View, Platform } from 'react-native';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 
 const MenuNavigator = createStackNavigator();
@@ -38,13 +38,55 @@ function MenuNavigatorScreen({ navigation }) {
 }
 
 
+const HomeNavigator = createStackNavigator();
+
+function HomeNavigatorScreen({ navigation }) {
+  return(
+      <HomeNavigator.Navigator
+          screenOptions={{
+              headerStyle: {
+                  backgroundColor: "#512DA8"
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                  color: "#fff"            
+              }
+          }}
+      >
+          <HomeNavigator.Screen
+              name="Home"
+              component={Home}
+          />        
+      </HomeNavigator.Navigator>
+  );
+}
+
+
+const Drawer = createDrawerNavigator();
+
+function MainNavigator() {
+  return(
+
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerStyle={{
+          backgroundColor: '#D1C4E9'
+        }} >
+          <Drawer.Screen name="Home" component={HomeNavigatorScreen} />
+          <Drawer.Screen name="Menu" component={MenuNavigatorScreen} />
+      </Drawer.Navigator>
+
+  );
+}
+
+
 class Main extends Component {
     
     render() { 
         return (  
           <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
             <NavigationContainer>
-              <MenuNavigatorScreen />
+              <MainNavigator />
             </NavigationContainer>
           </View>
           
